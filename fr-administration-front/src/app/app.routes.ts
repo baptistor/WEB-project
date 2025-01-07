b80f3963f626f73f103f74521ceebb2205c18ded
ykerverdo <yann.kerverdo@gmail.com>
2025-01-07 14:31:08 +0100

    registration

--- fr-administration-front/src/app/app.routes.ts
+++ fr-administration-front/src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { authGuard } from './guards/auth.guard';
 import { ProfileComponent } from './profile/profile.component';
 import { ModifyProfileComponent } from './modify-profile/modify-profile.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { notAuthGuard } from './guards/not-auth.guard';
 
 export const routes: Routes = [
-  {path: 'login', component: LoginComponent},
+  {path: 'login/:from', component: LoginComponent,canActivate: [notAuthGuard]},
+  {path: 'login', component: LoginComponent,canActivate: [notAuthGuard]},
   {path: 'users', component: UsersListComponent, canActivate: [authGuard]},
   {path: '', redirectTo:'login', pathMatch:'full'},
   {path: 'profile', component: ProfileComponent,canActivate: [authGuard]},
-  {path: 'modify-profile', component: ModifyProfileComponent,canActivate: [authGuard]}
+  {path: 'modify-profile', component: ModifyProfileComponent,canActivate: [authGuard]},
+  {path: 'registration', component: RegistrationComponent,canActivate: [notAuthGuard]}
 ];
